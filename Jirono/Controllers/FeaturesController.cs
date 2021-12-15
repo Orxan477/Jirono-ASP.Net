b0f2962b04b0d5f31d241deb6b73d0edc64a87bf
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-12-16 03:39:56 +0400

    Finish Front To Back

---
+++ Jirono/Controllers/FeaturesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jirono.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FeatureSingle()
+        {
+            return View();
+        }
+    }
+}
